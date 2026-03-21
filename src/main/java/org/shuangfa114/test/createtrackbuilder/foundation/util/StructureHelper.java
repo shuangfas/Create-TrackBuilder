a6083f236fd6707cc8f5f88823f1126565263a63
shuangfa114 <2124426292@qq.com>
2026-03-22 00:35:59 +0800

    结构转换调试

---
+++ src/main/java/org/shuangfa114/test/createtrackbuilder/foundation/util/StructureHelper.java
@@ -0,0 +1,130 @@
+package org.shuangfa114.test.createtrackbuilder.foundation.util;
+
+import com.simibubi.create.content.schematics.SchematicExport;
+import com.simibubi.create.foundation.utility.CreatePaths;
+import com.simibubi.create.foundation.utility.FilesHelper;
+import net.createmod.catnip.levelWrappers.SchematicLevel;
+import net.createmod.catnip.math.BBHelper;
+import net.minecraft.core.BlockPos;
+import net.minecraft.core.Vec3i;
+import net.minecraft.core.registries.Registries;
+import net.minecraft.nbt.CompoundTag;
+import net.minecraft.nbt.NbtAccounter;
+import net.minecraft.nbt.NbtIo;
+import net.minecraft.world.level.Level;
+import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.entity.BlockEntity;
+import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.level.levelgen.structure.templatesystem.StructurePlaceSettings;
+import net.minecraft.world.level.levelgen.structure.templatesystem.StructureTemplate;
+import org.shuangfa114.test.createtrackbuilder.CreateTrackBuilder;
+import org.shuangfa114.test.createtrackbuilder.mixin.accessor.PaletteInvoker;
+import org.shuangfa114.test.createtrackbuilder.mixin.accessor.StructureTemplateAccessor;
+
+import java.io.BufferedInputStream;
+import java.io.DataInputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.StandardOpenOption;
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.zip.GZIPInputStream;
+
+public class StructureHelper {
+    public static final Path CACHES_DIR = CreatePaths.SCHEMATICS_DIR.resolve("caches");
+
+    public static StructureTemplate toTemplate(SchematicLevel schematicLevel) {
+        return toTemplate(schematicLevel, BlockPos.ZERO);
+    }
+
+    public static StructureTemplate toTemplate(SchematicLevel schematicLevel, BlockPos anchor) {
+        StructureTemplate template = new StructureTemplate();
+        List<BlockPos> posList = new ArrayList<>();
+        List<BlockState> stateList = new ArrayList<>();
+        List<CompoundTag> nbtList = new ArrayList<>();
+        int[] min = new int[3];
+        int[] max = new int[3];
+        for (BlockPos origin : schematicLevel.getAllPositions()) {
+            BlockPos blockPos = origin.subtract(anchor);
+            int x = blockPos.getX();
+            int y = blockPos.getY();
+            int z = blockPos.getZ();
+            min[0] = Math.min(min[0], x);
+            min[1] = Math.min(min[1], y);
+            min[2] = Math.min(min[2], z);
+            max[0] = Math.max(max[0], Math.abs(x));
+            max[1] = Math.max(max[1], Math.abs(y));
+            max[2] = Math.max(max[2], Math.abs(z));
+            BlockEntity blockEntity = schematicLevel.getBlockEntity(origin);
+            //why add ,ask simibubi,lol about his logic
+            BlockState blockstate = schematicLevel.getBlockState(origin);
+            if (!blockstate.isAir()) {
+                CompoundTag tag = blockEntity == null ? null : blockEntity.saveWithId();
+                posList.add(blockPos);
+                stateList.add(blockstate);
+                nbtList.add(tag);
+            }
+        }
+        List<StructureTemplate.StructureBlockInfo> info = new LinkedList<>();
+        for (int i = 0; i < posList.size(); i++) {
+            info.add(new StructureTemplate.StructureBlockInfo(posList.get(i).offset(-min[0], -min[1], -min[2]), stateList.get(i), nbtList.get(i)));
+        }
+        ((StructureTemplateAccessor) template).getPalettes().add(PaletteInvoker.create(info));
+        ((StructureTemplateAccessor) template).setSize(new Vec3i(max[0] + 1, max[1] + 1, max[2] + 1));
+        return template;
+    }
+
+    public static SchematicExport.SchematicExportResult saveAsTemplate(StructureTemplate template, Path dir, String fileName, boolean overwrite) {
+        CompoundTag data = template.save(new CompoundTag());
+        if (fileName.isEmpty())
+            fileName = String.valueOf(data.hashCode());
+        if (!overwrite)
+            fileName = FilesHelper.findFirstValidFilename(fileName, dir, "nbt");
+        if (!fileName.endsWith(".nbt"))
+            fileName += ".nbt";
+        Path file = dir.resolve(fileName).toAbsolutePath();
+        try {
+            Files.createDirectories(dir);
+            boolean overwritten = Files.deleteIfExists(file);
+            try (OutputStream out = Files.newOutputStream(file, StandardOpenOption.CREATE)) {
+                NbtIo.writeCompressed(data, out);
+            }
+            return new SchematicExport.SchematicExportResult(file, dir, fileName, overwritten, null, null);
+        } catch (IOException e) {
+            CreateTrackBuilder.LOGGER.error("An error occurred while saving schematic [{}]", fileName, e);
+            return null;
+        }
+    }
+
+    public static StructureTemplate loadTemplate(Level level, Path path) {
+        StructureTemplate t = new StructureTemplate();
+        try (DataInputStream stream = new DataInputStream(new BufferedInputStream(
+                new GZIPInputStream(Files.newInputStream(path, StandardOpenOption.READ))))) {
+            CompoundTag nbt = NbtIo.read(stream, new NbtAccounter(0x20000000L));
+            t.load(level.holderLookup(Registries.BLOCK), nbt);
+        } catch (IOException e) {
+            CreateTrackBuilder.LOGGER.warn("Failed to read schematic", e);
+        }
+        return t;
+    }
+
+    public static SchematicLevel loadInSchematicLevel(Level origin, Path path, String fileName, BlockPos anchor) {
+        if (!fileName.endsWith(".nbt")) {
+            fileName += ".nbt";
+        }
+        StructureTemplate t = loadTemplate(origin, path.resolve(fileName));
+        SchematicLevel blockReader = new SchematicLevel(anchor, origin);
+        try {
+            t.placeInWorld(blockReader, anchor, anchor, new StructurePlaceSettings(), blockReader.getRandom(), Block.UPDATE_CLIENTS);
+        } catch (Exception e) {
+            CreateTrackBuilder.LOGGER.error("Failed to load Schematic for Printing", e);
+        }
+        BlockPos extraBounds = StructureTemplate.calculateRelativePosition(new StructurePlaceSettings(), new BlockPos(t.getSize())
+                .offset(-1, -1, -1));
+        blockReader.setBounds(BBHelper.encapsulate(blockReader.getBounds(), extraBounds));
+        return blockReader;
+    }
+}
